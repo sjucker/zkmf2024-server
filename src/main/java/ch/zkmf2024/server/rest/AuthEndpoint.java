315b67b69b68ab9029a77f112d1426ddb45dbf95
Stefan Jucker <stefan.jucker@gmail.com>
2021-03-20 18:37:51 +0100

    Let's try some logging

--- src/main/java/ch/zkmf2024/server/rest/AuthEndpoint.java
+++ src/main/java/ch/zkmf2024/server/rest/AuthEndpoint.java
@@ -4,12 +4,16 @@ import ch.zkmf2024.server.dto.RegisterRequestDto;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.UserRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/public/auth")
 public class AuthEndpoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthEndpoint.class);
 
     @GetMapping
     public ResponseEntity<String> login(String email, String password) {
@@ -18,6 +22,7 @@ public class AuthEndpoint {
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterRequestDto request) {
+        logger.info("/public/auth/register request: {}", request);
         UserRecord.CreateRequest createRequest = new UserRecord.CreateRequest();
         createRequest.setEmail(request.email());
         // createRequest.setPassword("alskdjaslkd jaksldjaslkd j");
