e0f3bc7b3cd37fc9400fd2aa9ba338ede98f771f
Stefan Jucker <jucker@mptechnology.ch>
2022-01-05 20:24:24 +0100

    Introduce PrimeNG

--- src/main/webapp/app/app.component.ts
+++ src/main/webapp/app/app.component.ts
@@ -1,10 +1,40 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MenuItem, PrimeIcons} from "primeng/api";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'webapp';
+export class AppComponent implements OnInit {
+
+    items: MenuItem[] = [];
+
+    ngOnInit(): void {
+        this.items = [
+            {
+                label: 'File',
+                items: [{
+                    label: 'New',
+                    icon: 'pi pi-fw pi-plus',
+                    items: [
+                        {label: 'Project'},
+                        {label: 'Other'},
+                    ]
+                },
+                    {label: 'Open', icon: PrimeIcons.CARET_LEFT},
+                    {label: 'Quit'}
+                ]
+            },
+            {
+                label: 'Edit',
+                icon: 'pi pi-fw pi-pencil',
+                items: [
+                    {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+                    {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+                ]
+            }
+        ];
+    }
+
 }
