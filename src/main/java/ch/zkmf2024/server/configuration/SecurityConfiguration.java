15f2a5904a451dab58dc161f73c8ba307fcdb052
Stefan Jucker <jucker@mptechnology.ch>
2022-10-31 13:55:33 +0100

    Update SecurityConfiguration

--- src/main/java/ch/zkmf2024/server/configuration/SecurityConfiguration.java
+++ src/main/java/ch/zkmf2024/server/configuration/SecurityConfiguration.java
@@ -1,16 +1,13 @@
 package ch.zkmf2024.server.configuration;
 
 import ch.zkmf2024.server.security.JwtRequestFilter;
-import ch.zkmf2024.server.security.UserAuthService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -18,30 +15,20 @@ import org.springframework.web.filter.CorsFilter;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.OPTIONS;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
     private final JwtRequestFilter jwtRequestFilter;
-    private final UserAuthService userAuthService;
 
-    public SecurityConfiguration(JwtRequestFilter jwtRequestFilter, UserAuthService userAuthService) {
+    public SecurityConfiguration(JwtRequestFilter jwtRequestFilter) {
         this.jwtRequestFilter = jwtRequestFilter;
-        this.userAuthService = userAuthService;
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring()
-           .antMatchers("/public/**")
-           .antMatchers(OPTIONS);
     }
 
     @Bean
@@ -56,13 +43,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new CorsFilter(source);
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userAuthService).passwordEncoder(passwordEncoder());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.httpBasic().disable()
             .cors().and()
             .csrf().disable()
@@ -76,6 +58,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .and().sessionManagement().sessionCreationPolicy(STATELESS);
 
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 
 
