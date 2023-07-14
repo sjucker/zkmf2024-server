129148750ccd4aeafc9e3dc2005648c29084e67b
Stefan Jucker <jucker@mptechnology.ch>
2023-07-14 11:06:04 +0200

    Add missing annotation

--- src/main/java/ch/zkmf2024/server/ServerApplication.java
+++ src/main/java/ch/zkmf2024/server/ServerApplication.java
@@ -2,7 +2,9 @@ package ch.zkmf2024.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ServerApplication {
 
