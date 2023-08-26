e50bbd5d2adbd5567e70b614c422e9f6255bd15f
Stefan Jucker <stefan.jucker@gmail.com>
2023-08-26 15:52:28 +0200

    Code cleanup

--- src/main/java/ch/zkmf2024/server/configuration/SecurityConfiguration.java
+++ src/main/java/ch/zkmf2024/server/configuration/SecurityConfiguration.java
@@ -49,21 +49,20 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.httpBasic(AbstractHttpConfigurer::disable)
-            .cors(withDefaults())
-            .csrf(AbstractHttpConfigurer::disable)
-            .formLogin(AbstractHttpConfigurer::disable)
-            .logout(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(registry -> {
-                registry.requestMatchers(new AntPathRequestMatcher("/public/**")).permitAll();
-                registry.requestMatchers(new AntPathRequestMatcher("/secured/**")).authenticated();
-                registry.anyRequest().permitAll();
-            })
-            .exceptionHandling(withDefaults())
-            .sessionManagement(configurer -> configurer.sessionCreationPolicy(STATELESS))
-            .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
+        return http.httpBasic(AbstractHttpConfigurer::disable)
+                   .cors(withDefaults())
+                   .csrf(AbstractHttpConfigurer::disable)
+                   .formLogin(AbstractHttpConfigurer::disable)
+                   .logout(AbstractHttpConfigurer::disable)
+                   .authorizeHttpRequests(registry -> {
+                       registry.requestMatchers(new AntPathRequestMatcher("/public/**")).permitAll();
+                       registry.requestMatchers(new AntPathRequestMatcher("/secured/**")).authenticated();
+                       registry.anyRequest().permitAll();
+                   })
+                   .exceptionHandling(withDefaults())
+                   .sessionManagement(configurer -> configurer.sessionCreationPolicy(STATELESS))
+                   .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+                   .build();
     }
 
 }
