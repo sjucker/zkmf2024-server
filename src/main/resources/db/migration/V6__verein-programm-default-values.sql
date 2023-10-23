aebebbb27f4bc1f6ae563f19eaf16da6c9dd2d36
Stefan Jucker <stefan.jucker@gmail.com>
2023-10-23 15:13:21 +0200

    Add default values for booleans in programm

---
+++ src/main/resources/db/migration/V6__verein-programm-default-values.sql
@@ -0,0 +1,10 @@
+alter table verein_programm
+    alter column modul_b_pa set default false;
+alter table verein_programm
+    alter column modul_b_egitarre set default false;
+alter table verein_programm
+    alter column modul_b_ebass set default false;
+alter table verein_programm
+    alter column modul_b_keyboard set default false;
+alter table verein_programm
+    alter column modul_b_gesang set default false;
