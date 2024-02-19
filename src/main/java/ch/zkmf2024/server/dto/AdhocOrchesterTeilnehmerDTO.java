51850624f7ad23b6a762147ee27f10e495722fb4
Stefan Jucker <stefan.jucker@gmail.com>
2024-02-19 10:25:17 +0100

    Ignore property

--- src/main/java/ch/zkmf2024/server/dto/AdhocOrchesterTeilnehmerDTO.java
+++ src/main/java/ch/zkmf2024/server/dto/AdhocOrchesterTeilnehmerDTO.java
@@ -1,11 +1,14 @@
 package ch.zkmf2024.server.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 public record AdhocOrchesterTeilnehmerDTO(String name,
                                           String email,
                                           String instrument) {
 
+    @JsonIgnore
     public boolean isNotEmpty() {
         return isNotBlank(name) && isNotBlank(email) && isNotBlank(instrument);
     }
