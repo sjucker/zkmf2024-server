39bb088457ae8c14380207da62df87ba15c98937
Stefan Jucker <stefan.jucker@gmail.com>
2024-02-29 22:08:03 +0100

    Festzelt updates

---
+++ src/main/resources/db/migration/V30__festzelt.sql
@@ -0,0 +1,11 @@
+update location
+set location_type = 'FESTZELT',
+    name          = 'Festzelt Schlieren',
+    map_id        = 'FS'
+where id = 21;
+
+update location
+set location_type = 'FESTZELT',
+    name          = 'Festzelt Urdorf',
+    map_id        = 'FU'
+where id = 22;
